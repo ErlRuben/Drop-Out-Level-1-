c7d477203c7beb532ed5015777418662e6055d39
Earl Batenga <61070797+Elchupappi@users.noreply.github.com>
2020-10-03 11:01:22 +0800

    First Sample
    Sca-Fall-Ding

---
+++ Sca-fall-ding/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Transform trans;
+    public float speed= 34f;
+    public float range = 70f;
+    private Vector3 spawnPoint;
+    void Start()
+    {
+        spawnPoint = trans.position;
+    }
+
+    
+    void Update()
+    {
+        trans.Translate(0, 0, speed*Time.deltaTime, Space.Self);
+
+        if (Vector3.Distance(trans.position, spawnPoint)>=range)
+        {
+          //  Destroy(gameObject);
+        }
+    }
+}
